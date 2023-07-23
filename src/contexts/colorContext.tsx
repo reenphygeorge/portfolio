1614cf1725f83b878f83097dc211e59571848940
Reenphy George <reenphygeorge@gmail.com>
2023-07-23 15:02:44 +0530

    fix: updated color context

--- src/contexts/colorContext.tsx
+++ src/contexts/colorContext.tsx
@@ -14,15 +14,15 @@ type GlobalColors = {
 };
 
 const ColorContext = createContext<GlobalColors>({
-  navBg: "string",
-  green50: "string",
-  red50: "string",
-  black50: "string",
-  menuItem: "string",
-  black25: "string",
-  white: "string",
-  gray50: "string",
-  unselectedRouteBg: "string",
+  navBg: "rgba(255, 255, 255, 0.15)",
+  green50: "green.50",
+  red50: "red.50",
+  black50: "black.50",
+  menuItem: "black.50",
+  black25: "black.25",
+  white: "white",
+  gray50: "gray.50",
+  unselectedRouteBg: "black.25",
 });
 
 const ColorProvider: FC<PropsWithChildren<{}>> = ({ children }) => {
